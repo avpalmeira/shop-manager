7bea31d69aedc145cfc18b1f09a10a656872c707
Antonio Palmeira <av.palmeira@gmail.com>
2020-09-26 19:22:44 -0300

    home page with signin, signup and logout

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,10 @@
 Rails.application.routes.draw do
+  root to: 'home#index'
   resources :products
-  devise_for :users
+  devise_for :users, skip: [:sessions]
+	as :user do
+		get 'signin', to: 'devise/sessions#new', as: :new_user_session
+		post 'signin', to: 'devise/sessions#create', as: :user_session
+		match 'signout', to: 'devise/sessions#destroy', as: :destroy_user_session, via: Devise.mappings[:user].sign_out_via
+	end
 end
