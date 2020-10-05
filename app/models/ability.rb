315e02766b30aa7cacf97416c2e6549b96b0350c
Antonio Palmeira <av.palmeira@gmail.com>
2020-10-05 11:33:24 -0300

    limit access to products only to signed in users

--- app/models/ability.rb
+++ app/models/ability.rb
@@ -2,11 +2,14 @@ class Ability
   include CanCan::Ability
 
   def initialize(user)
-    user ||= User.new # guest user (not logged in)
-    if user.admin?
-      can :manage, :all
-    else
-      can :read, :all
+    # user ||= User.new # guest user (not logged in)
+
+    if user
+      if user.admin?
+        can :manage, :all
+      else
+        can :read, :all
+      end
     end
   end
 end
