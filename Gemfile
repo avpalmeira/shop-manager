2e3b0214d682e5e94a362921f6592790dcebc3b0
Antonio Palmeira <av.palmeira@gmail.com>
2020-10-10 14:54:40 -0300

    configure cors

--- Gemfile
+++ Gemfile
@@ -29,6 +29,8 @@ gem 'dotenv-rails'
 gem 'cancancan'
 # Use Amazon S3
 gem 'aws-sdk-s3', require: false
+# Configure CORS
+gem 'rack-cors'
 
 # Use Active Storage variant
 # gem 'image_processing', '~> 1.2'
---
+++ config/initializers/cors.rb
@@ -0,0 +1,20 @@
+# Be sure to restart your server when you modify this file.
+
+# Allow CORS to all origins
+Rails.application.config.middleware.insert_before 0, Rack::Cors do
+  allow do
+    origins '*'
+
+    resource '*',
+      :headers => :any,
+      :methods => [
+        :get,
+        :post,
+        :delete,
+        :head,
+        :options
+      ],
+      :expose => ['Content-Range']
+  end
+end
+
