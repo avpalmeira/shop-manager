f884e6c45952e83482c774f83e59f7e08407384f
Antonio Palmeira <av.palmeira@gmail.com>
2020-10-21 20:18:54 -0300

    try: resolve csrf token verification for auth

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,15 +1,8 @@
 class ApplicationController < ActionController::Base
         include DeviseTokenAuth::Concerns::SetUserByToken
-  before_action :configure_permitted_parameters, if: :devise_controller?
   protect_from_forgery unless: -> { request.format.json? }
 
   rescue_from CanCan::AccessDenied do |exception|
     redirect_to root_url, :alert => exception.message
-  end
-
-  protected
-
-  def configure_permitted_parameters
-    devise_parameter_sanitizer.permit(:sign_up, keys: [:admin])
   end
 end
