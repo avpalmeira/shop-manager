2e0f2a1df814960582c11b89f71576a0c29a591a
Antonio Palmeira <av.palmeira@gmail.com>
2020-10-22 10:10:35 -0300

    use old host url and restrict cors origins

--- config/initializers/cors.rb
+++ config/initializers/cors.rb
@@ -3,7 +3,8 @@
 # Config CORS
 Rails.application.config.middleware.insert_before 0, Rack::Cors do
   allow do
-    origins '*'
+    origins 'localhost:3000',
+      'franqueados-web.herokuapp.com'
 
     resource '*',
       :headers => :any,
