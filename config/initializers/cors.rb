072ed31ce4cbb8cbc44ca5235be49fe2c2203e50
Antonio Palmeira <av.palmeira@gmail.com>
2020-10-22 09:35:45 -0300

    update cors origins

--- config/initializers/cors.rb
+++ config/initializers/cors.rb
@@ -4,7 +4,7 @@
 Rails.application.config.middleware.insert_before 0, Rack::Cors do
   allow do
     origins 'localhost:8000',
-      'https://franqueados-web.herokuapp.com'
+      'https://franqueados.herokuapp.com'
 
     resource '*',
       :headers => :any,
