dcd015488b6b7e3b78ff6a43312dd24f3cdc6d35
Antonio Palmeira <av.palmeira@gmail.com>
2020-10-21 21:34:56 -0300

    change old layout views

--- config/routes.rb
+++ config/routes.rb
@@ -1,8 +1,8 @@
 Rails.application.routes.draw do
   mount_devise_token_auth_for 'User', at: 'auth'
   root to: 'home#index'
-  get 'products/decode', to: 'products#decode'
   defaults format: :json do
+    get 'products/decode', to: 'products#decode'
     resources :products
   end
 end
